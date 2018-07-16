ce6255003db3b9902343502cdf32934aa5ae525a
Sihem <sihem@mac-mini-de-msf.local>
2018-07-16 13:52:43 +0200

    Initial Commit

---
+++ ClearSky/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ClearSky
+//
+//  Created by Sihem on 16/07/2018.
+//  Copyright © 2018 Sihem. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
