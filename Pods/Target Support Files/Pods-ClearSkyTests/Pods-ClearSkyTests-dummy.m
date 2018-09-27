149dc294387929c44e58477fffd90accd7b4e47f
System Administrator <root@Mac-mini-de-MSF.local>
2018-09-27 18:25:35 +0200

    Remove dependencies folder

--- Pods/Target Support Files/Pods-ClearSkyTests/Pods-ClearSkyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ClearSkyTests : NSObject
-@end
-@implementation PodsDummy_Pods_ClearSkyTests
-@end
--- Pods/Target Support Files/Pods-ClearSkyUITests/Pods-ClearSkyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ClearSkyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ClearSkyUITests
-@end
